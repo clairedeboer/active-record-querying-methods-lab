5e6ea4ad0df9c6ad826b02bd94f1aa62868b75ee
clairedeboer <claire.deboer@gmail.com>
2020-11-28 20:29:32 -0500

    ar querying methods lab

--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,8 @@ require 'yaml'
 require 'active_record'
 
 Bundler.require
-
-Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
+Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| 
+require f}
 Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
 
 DB = ActiveRecord::Base.establish_connection({
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@ ENV["ACTIVE_RECORD_ENV"] = "test"
 require_relative '../config/environment'
 
 RSpec.configure do |config|
-
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
     DatabaseCleaner.start
